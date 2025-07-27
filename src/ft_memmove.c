ffaa195863071a3229b55612e9f2d6eadfad5583
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-07-27 04:24:33 -0300

    Create ft_memmove.c

---
+++ src/ft_memmove.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*ptrdest;
+	unsigned const char	*ptrsrc;
+
+	ptrdest = (unsigned char *)dest;
+	ptrsrc = (unsigned const char *)src;
+	i = 0;
+	if (dest > src)
+	{
+		while (n--)
+			ptrdest[n] = ptrsrc[n];
+	}
+	else
+	{
+		while (i < n)
+		{
+			ptrdest[i] = ptrsrc[i];
+			i++;
+		}
+	}
+	return (dest);
+}
