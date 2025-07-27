2f15014101d7caf6d12d8c3a84a07f168684b57e
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-07-27 05:23:00 -0300

    Create ft_strjoin.c

---
+++ src/ft_strjoin.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	len;
+	char	*join;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	len = s1_len + s2_len + 1;
+	join = malloc(len);
+	if (!join)
+		return (NULL);
+	ft_memcpy(join, s1, s1_len);
+	ft_memcpy(join + s1_len, s2, s2_len + 1);
+	return (join);
+}
