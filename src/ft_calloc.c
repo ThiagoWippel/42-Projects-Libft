c3a9bb4efc511947273611d093ee3ff836ceb09f
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-07-27 04:30:11 -0300

    Create ft_calloc.c

---
+++ src/ft_calloc.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total;
+	void	*ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	total = nmemb * size;
+	if (nmemb != 0 && total / nmemb != size)
+		return (NULL);
+	ptr = malloc(total);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, total);
+	return (ptr);
+}
