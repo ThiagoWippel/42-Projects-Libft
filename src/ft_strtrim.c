1497882aed3722634da8d11f0406b13c77cea1a8
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-07-27 05:54:16 -0300

    Create ft_strtrim.c

---
+++ src/ft_strtrim.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+	size_t	end;
+	size_t	start;
+	char	*trimmed;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	len = end - start;
+	trimmed = malloc(len + 1);
+	if (!trimmed)
+		return (NULL);
+	ft_memcpy(trimmed, s1 + start, len);
+	trimmed[len] = '\0';
+	return (trimmed);
+}
