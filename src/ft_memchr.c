a1758cf5cecfdc78f8a0344d5ea6f4dbf7fa6441
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-07-27 04:28:15 -0300

    Create ft_memchr.c

---
+++ src/ft_memchr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *str, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)str;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char)c)
+			return (ptr + i);
+		i++;
+	}
+	return (NULL);
+}
