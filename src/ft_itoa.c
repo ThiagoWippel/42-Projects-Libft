827c80fe3822be8dbf6ee68bf8dca6ac0a1c6b86
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-07-30 00:05:15 -0300

    Create ft_itoa.c

---
+++ src/ft_itoa.c
@@ -0,0 +1,50 @@
+#include "libft.h"
+
+size_t	count_digits(int n)
+{
+	long	num;
+	size_t	digits;
+
+	num = n;
+	digits = 1;
+	if (n < 0)
+	{
+		num = -num;
+		digits++;
+	}
+	while (num >= 10)
+	{
+		num /= 10;
+		digits++;
+	}
+	return (digits);
+}
+
+char	*ft_itoa(int n)
+{
+	size_t	i;
+	long	num;
+	char	*str;
+	size_t	digits;
+
+	num = n;
+	digits = count_digits(n);
+	str = malloc(digits + 1);
+	if (!str)
+		return (NULL);
+	str[digits] = '\0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		num = -num;
+	}
+	i = digits - 1;
+	while (num >= 10)
+	{
+		str[i] = (num % 10) + '0';
+		num /= 10;
+		i--;
+	}
+	str[i] = num + '0';
+	return (str);
+}
