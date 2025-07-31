d4b738f65899386c6131fff0b74962b856922f17
Thiago Wippel Chaves <141747751+ThiagoWippel@users.noreply.github.com>
2025-07-31 01:39:26 -0300

    Update ft_memcpy.c

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,6 +6,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*ptrdest;
 	unsigned const char	*ptrsrc;
 
+	if (n == 0 || dest == src)
+		return (dest);
+	if (!dest && !src)
+		return (dest);
 	ptrdest = (unsigned char *)dest;
 	ptrsrc = (unsigned const char *)src;
 	i = 0;
